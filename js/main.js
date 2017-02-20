9daeed459ccd0c4452cb3e694ecf5d89a873f29c
arangodavid <arango.david8@gmail.com>
2017-02-19 21:07:49 -0500

    Enjoy

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var numsArray = [];
+// var total = 0;
+//
+// function beautiful(multiple1,multiple2,upwardBound){
+//   	for(var n=0;n<=upwardBound;n++){
+// 		if (n % multiple1 === 0 || n % multiple2 === 0){
+// 			numsArray.push(n);
+//   	}
+// 	}
+//   sum(numsArray);
+// }
+//
+// 	function sum(multiArr){
+// 		multiArr.forEach(function(a){
+//       total += a;
+//     });
+//    console.log(total);
+//   }
+//
+// beautiful(3,5,1000);
