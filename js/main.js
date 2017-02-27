5a09a518a9ca4037b51b0228fcb6adf303c30ddf
arangodavid <arango.david8@gmail.com>
2017-02-27 14:17:39 -0500

    Added weather API

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 $(document).ready(function (){
   console.log("ready");
 
-  var key = "https://crossorigin.me/https://api.darksky.net/forecast/53bae4534b0da5b692246d4e1623a959/37.8267,-122.4233";
+  var key = "https://api.darksky.net/forecast/53bae4534b0da5b692246d4e1623a959/37.8267,-122.4233";
   $("body").on("click", function(){
       console.log("clicked");
       $.ajax({
         url: key,
+        dataType: "jsonp",
         success: function(r){
           console.log("success")
           console.log(r);
           var temperature = r.currently.temperature;
-          $("ul").append("<li>" + temperature + " degrees</li>")
+          $("ul").append("<li>" + temperature + "</li>")
         },
         error: function(e) {
           console.log("wrong")
